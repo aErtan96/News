10f4b060e4bf22c76fd42328096f9548d1b0d349
eraydertsiz <edertsiz1@gmail.com>
2020-04-17 14:31:38 +0300

    Article Fragment Yapıldı

---
+++ app/src/main/java/com/dertsizvebugsiz/news/fragments/ArticleFragment.java
@@ -0,0 +1,22 @@
+package com.dertsizvebugsiz.news.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dertsizvebugsiz.news.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ArticleFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_article, container, false);
+        return v;
+    }
+}
